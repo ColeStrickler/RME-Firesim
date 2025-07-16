0bfe6429843286473368c8794b2b823bff2490ce
ColeStrickler <colestrickler@gmail.com>
2025-07-16 13:42:13 -0500

    requestor changes

--- Requestor.scala
+++ Requestor.scala
@@ -12,6 +12,8 @@ import freechips.rocketchip.diplomacy.BufferParams.flow
 import _root_.subsystem.rme.subsystem.rme.IDAllocator
 import scala.annotation.meta.param
 import os.stat
+import agu.AGUTop
+import agu.AGUParams
 
 
 
@@ -72,7 +74,8 @@ class RequestorRME(params: RelMemParams, tlInEdge : TLEdge, tlOutEdge: TLEdge, t
 
         // this isn't entirely flexible, assumes 1 bit source expansion
         val id_allocator = Module(new IDAllocator(math.pow(2, tlInParams.sourceBits-1).toInt, maxID))
-
+        val agu = LazyModule(new AGUTop(new AGUParams))
+        
 
 
         val DatabaseBaseAddress = params.rmeaddress.U
@@ -90,15 +93,8 @@ class RequestorRME(params: RelMemParams, tlInEdge : TLEdge, tlOutEdge: TLEdge, t
         val ModifiedRequestsSent = WireInit(true.B) // track if we have sent all the necessary requests
         val readyNextReq = Wire(Bool())
 
-        // What happens if enabled column count changes while we're handling request? --> we can probably relax this assumption
-        // I think this will lead to issues of incomplete request formation
-        // We will load a new config here so it does not get modified mid request generation
-        val CurrentRowSize = RegInit(0.U(32.W)) // size of each row in database
-        val CurrentRowCount = RegInit(0.U(32.W))  // count of each row in database
-        val CurrentEnabledColumnCount = RegInit(0.U(4.W))  // total number of enabled columns
-        val CurrentColumnWidth = RegInit(0.U(7.W)) // width of ith enabled column
-        val CurrentColumnOffsets =  RegInit(VecInit(Seq.fill(15)(0.U(7.W)))) // offset off column j from column j-1
-        val CurrentFrameOffset = RegInit(0.U(32.W))
+
+
 
         // This should give us the total size in bytes we need to grab
         val TotalCacheLinesNeeded = RegInit(0.U(8.W))
@@ -109,10 +105,11 @@ class RequestorRME(params: RelMemParams, tlInEdge : TLEdge, tlOutEdge: TLEdge, t
         val nDescriptors = RegInit(0.U(8.W))
         nDescriptors := 64.U/io.Config.ColumnWidths
         val requestOffset = (requestQueue.io.deq.bits.address - params.rmeaddress.U)(31, 0)
-        val requestRow = requestOffset / sumColWidths//requestOffset / io.Config.RowSize.pad(33)//(requestOffset - (requestOffset % io.Config.RowSize))
-        val row = RegInit(0.U(log2Ceil(params.rmeAddressSize).W))
         //row := requestRow
         val nDescriptorsSent = RegInit(0.U(8.W))
+        val nSentForProcessing = RegInit(0.U(8.W))
+
+        
         val col = RegInit(0.U(log2Ceil(512 + 1).W))
         val sumOffset = RegInit(0.U(log2Ceil(512 + 1).W))
         val busWidth = 8.U(7.W)
@@ -169,35 +166,25 @@ class RequestorRME(params: RelMemParams, tlInEdge : TLEdge, tlOutEdge: TLEdge, t
             is (idle)
             {
                 nDescriptorsSent := 0.U
-                col := 0.U
-                sumOffset := 0.U
-                row := requestRow
+                nSentForProcessing := 0.U
                 stateReg := Mux(requestQueue.io.deq.fire, active, idle)
                 baseRequest := requestQueue.io.deq.bits
-
-                
-
             }
             is (active)
             {
                 //SynthesizePrintf("Rowsize %d, row %d, io.Config.ColumnOffsets(col) %d\n", io.Config.RowSize, row, io.Config.ColumnOffsets(col))
-                
-                
+                agu.module.io.doGen.valid := (nSentForProcessing < nDescriptors)
+                //agu.module.io.doGen.fire
+                //agu.module.io.offset.fire
+                        
                 val last = col === io.Config.EnabledColumnCount - 1.U
                 val done = last && outQueue.io.enq.fire
-                val P_i_j = (io.Config.RowSize * row) + (sumOffset + io.Config.ColumnOffsets(col))
                 //SynthesizePrintf("[REQUESTOR] baseRequest.address 0x%x\n", baseRequest.address)
+                val P_i_j = agu.module.io.offset.bits
                 val R_i_j = (P_i_j / 8.U(60.W)) * busWidth
-                val nBeats = divideCeil((P_i_j % busWidth) + io.Config.ColumnWidths, 8.U(60.W))
+                val nBeats = 1.U //divideCeil((P_i_j % busWidth) + io.Config.ColumnWidths, 8.U(60.W))
                 val sizeField = OHToUInt(nBeats * 8.U) // need to check this, this should usually turn out fine with col size < 16
                 val discardFront = P_i_j % busWidth
-
-
-                /*
-                    We altered this from the EDBT paper
-   
-                    This needs edited -> causing freeze with multi-column 
-                */
                 val busAlignment = ((P_i_j + io.Config.ColumnWidths) % busWidth)
                 val discardBack = (R_i_j + nBeats*busWidth - (P_i_j + io.Config.ColumnWidths))//Mux(io.Config.ColumnWidths < 8.U, busWidth - busAlignment, busAlignment) 
 
@@ -216,13 +203,13 @@ class RequestorRME(params: RelMemParams, tlInEdge : TLEdge, tlOutEdge: TLEdge, t
                 descriptorOut.discardFront := discardFront
                 descriptorOut.discardBack := discardBack
                 descriptorOut.beatCount := nBeats
-                assert(nBeats > 0.U && nBeats <= 5.U);
+                assert(nBeats > 0.U && nBeats <= 5.U)
 
                 outQueue.io.enq.bits.FetchReq := sendRequest.bits
                 outQueue.io.enq.bits.descriptor := descriptorOut
                 outQueue.io.enq.bits.BaseReq := baseRequest
-                outQueue.io.enq.valid :=  sendRequest.valid && id_allocator.io.newID.fire
-                
+                outQueue.io.enq.valid :=  sendRequest.valid && id_allocator.io.newID.fire && agu.module.io.offset.valid
+                agu.module.io.offset.ready := outQueue.io.enq.ready
 
                 when (outQueue.io.enq.fire)
                 {
@@ -234,96 +221,13 @@ class RequestorRME(params: RelMemParams, tlInEdge : TLEdge, tlOutEdge: TLEdge, t
                     SynthesizePrintf("[REQUESTOR] sent %d/%d\n", nDescriptorsSent, nDescriptors)
                 }
 
+
+                nSentForProcessing := nSentForProcessing + agu.module.io.doGen.fire
                 nDescriptorsSent := nDescriptorsSent + outQueue.io.enq.fire
                 sumOffset := Mux(outQueue.io.enq.fire, Mux(last, 0.U, sumOffset + io.Config.ColumnOffsets(col)), sumOffset)
-                col := Mux(outQueue.io.enq.fire, Mux(last, 0.U, col + 1.U), col)
-                row := Mux(done, row + 1.U, row)
+
                 stateReg := Mux(nDescriptorsSent === nDescriptors - 1.U && outQueue.io.enq.fire, idle, stateReg)
             }
         }
-        
-        
 
-
-        //when (io.FetchUnit.fire)
-        //{
-        //    SynthesizePrintf("[REQUESTOR] ==> Sent request to fetch unit base src: %d, alloc src %d\n", baseRequest.source, id_allocator.io.newID.bits)
-        //}
-  
-        
-
-
-        /*
-        // Next state logic
-        switch(stateReg)
-        {
-            is (idle) {
-                // When we have a new request we are now active
-                stateReg := Mux(!ModifiedRequestsSent, active, idle)
-
-                // when we receive a new request in, we have no longer sent all requests
-                ModifiedRequestsSent := Mux(requestQueue.io.deq.fire, false.B, true.B)
-                baseRequest := requestQueue.io.deq.bits
-
-
-                /*
-                    Update config
-                */
-                CurrentRowSize              := io.Config.RowSize
-                CurrentRowCount             := io.Config.RowCount
-                CurrentEnabledColumnCount   := io.Config.EnabledColumnCount
-                CurrentColumnWidths         := io.Config.ColumnWidths
-                CurrentColumnOffsets        := io.Config.ColumnOffsets
-                CurrentFrameOffset          := io.Config.FrameOffset
-                // Computer how much data we need to fetch to construct a single cache line
-                val singleRowEnColSize = (io.Config.ColumnWidths * io.Config.EnabledColumnCount)
-                val rowsNeeded = divideCeil(64.U, singleRowEnColSize)
-                val cacheLinesNeeded = divideCeil(rowsNeeded*io.Config.RowSize, 64.U(64.W))
-                TotalCacheLinesNeeded       := 4.U//cacheLinesNeeded
-                TotalCacheLinesSent         := 0.U
-                id_allocator.io.newID.ready := false.B
-
-            }
-            is (active) {
-                /*
-                    If we have sent all the necessary requests, we transition back to idle state
-                */
-                stateReg := Mux(ModifiedRequestsSent, idle, active)
-
-                baseRequest := baseRequest // keep base request for all active states
-
-                /*
-                    Send requests to fetch unit
-                */
-                val sendRequest = Wire(Valid(new TLBundleA(tlInParams)))
-                sendRequest.bits := baseRequest
-                // cache line size = 64 bytes, so we increment each request by 0x40
-                sendRequest.bits.address := baseRequest.address + (TotalCacheLinesSent * 0x40.U)
-                sendRequest.valid := true.B && !readyNextReq
-                io.FetchUnit.bits.FetchReq := sendRequest.bits
-                io.FetchUnit.bits.FetchReq.size := log2Ceil(16).U // size is log2(opsize) --> request at bus width granularity
-                io.FetchUnit.valid := sendRequest.valid && id_allocator.io.newID.fire
-                id_allocator.io.newID.ready := io.FetchUnit.ready // circular logic?
-                io.FetchUnit.bits.isBaseRequest := (TotalCacheLinesSent === 0.U) // first req
-
-
-                /*
-                    Update config before generating next request
-                */
-                CurrentRowSize              := Mux(ModifiedRequestsSent, io.Config.RowSize, CurrentRowSize)
-                CurrentRowCount             := Mux(ModifiedRequestsSent, io.Config.RowCount, CurrentRowCount)
-                CurrentEnabledColumnCount   := Mux(ModifiedRequestsSent, io.Config.EnabledColumnCount, CurrentEnabledColumnCount)
-                CurrentColumnWidths         := Mux(ModifiedRequestsSent, io.Config.ColumnWidths, CurrentColumnWidths)
-                CurrentColumnOffsets        := Mux(ModifiedRequestsSent, io.Config.ColumnOffsets, CurrentColumnOffsets)
-                CurrentFrameOffset          := Mux(ModifiedRequestsSent, io.Config.FrameOffset, CurrentFrameOffset)
-                // if we have an offset > 64 we can skip a line)
-                TotalCacheLinesNeeded       := Mux(ModifiedRequestsSent, // maybe an error?
-                    0.U, TotalCacheLinesNeeded) 
-                TotalCacheLinesSent         := Mux(!io.FetchUnit.fire, TotalCacheLinesSent,
-                    Mux(TotalCacheLinesSent < TotalCacheLinesNeeded - 1.U, TotalCacheLinesSent + 1.U, 0.U))
-                ModifiedRequestsSent        := Mux(TotalCacheLinesSent === (TotalCacheLinesNeeded - 1.U) && 
-                    io.FetchUnit.fire, true.B, false.B)
-            }
-        }
-            */
 }
